a1687bd4f22a8170221020c83af6030b75acf499
root <root@7d52c56892ae>
2021-09-11 18:35:51 +0000

    crazy cat lady, high lo game

--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -8,6 +8,36 @@ import javax.swing.JOptionPane;
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
+		// the code starts off asking the user for a number
+		// representing how many cats.  a whole number is
+		// stored in a variable of type 'int'.
+		// Unfortunately JOptionPane returns the user's
+		// answer as a String which is a very different
+		// data type.  it's hard to explain.  i wish I had
+		// a whiteboard to draw on!  let's say for example
+		// the user has 935 cats :) ... storing the number
+		// as an 'int' is different than as a String.  as
+		// a String it gets stored as a list of digits: 9, 3, 5
+		// see how different that is?yes
+		
+		// ok.  so manipulating the number 935 as an int
+		// is different code than manipulating it as a String.
+		// as a String, all you can do is check the list of
+		// digits matches, e.g.:
+		//String user = "935";
+		//if (user.equals("935"))	// this would test if
+			// the String user has the list 9, 3, 5
+			
+		// down below in your code, we converted the String
+			// to an 'int'
+		//	int catsInt = Integer.parseInt(user);
+			// so now we can do number comparisons with it
+			// like is it equal to a number or greater than
+			// or less than
+		//if (catsInt == 935)..
+		//if (catsInt > 0)
+			
+
 		// 1. Ask the user how many cats they have
 		String user = JOptionPane.showInputDialog("how many cats do you have?");
 		
@@ -46,21 +76,45 @@ public class CrazyCatLady {
 		// i'm going to check on mariene
 
 // CLASS IS ALMOST OVER>  LETS SAVE WORK AND CONTINUE FIXING THIS SECTION NEXT TIME OK?
+		// hmm it looks like your latest code wasn't saved!!
+		
+		// maybe you can fix the code again - good practice!
+		
+		// there are 2 things:
+		// 1.  the test conditions for the 2nd and 3rd if's
+		// are wrong, and
+		// 2.  it would be more efficient to do an 
+		// "if -else if" series
+		
 		// want help saving work?yes
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-		if (catsInt >= 3);	// can you fix the test?  good start but don't use 'user' here!
+		if (catsInt >= 3)	// can you fix the test?  good start but don't use 'user' here!
 		// need help?yes pls.  notice NO double quotes here.  that's only for String stuff
 				System.out.println("You are a crazy cat lady.");
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 		//    Hint: You can use the && operator 
-		if (user.equals("-3 catsInt"));
+		else if (catsInt < 3 && catsInt > 0)
 				System.out.print("https://www.youtube.com/watch?v=QtC3Bo9B0yI");
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		if (user.equals("0 catsInt"));
+		else if (catsInt==0) {
 				System.out.println ("https://www.youtube.com/watch?v=oj_yLBltPE8");
+				playVideo("https://www.youtube.com/watch?v=oj_yLBltPE8");
+
+		}
+				// another problem .. there should be no semicolon
+				// on an 'if' line of code
+				
+// i'm back.  want help with line 96?yes .  to test
+		// the value in catsInt using an AND, each
+		// test needs to be separate. let me show you.
+		// do you follow?  good.  now i'll just clean up
+		// some indentation..done!  want to try it? try what
+		// run the program
 		
+		// ok, the code works.  i'm not sure if we were
+		// able to get a video to run.  i'll test this
+		// now, but i don't think it works.
 		
-		// hi miranda.  how's it going?well im done but im not sure if the the if statements are correct
 				
 				// ok.  so a few things i'd like to address.  first when you do if (something)
 				// the something is actually called a test condition.  in this program
@@ -82,4 +136,3 @@ public class CrazyCatLady {
 	}
 
 }
-
--- src/_01_else_if/_3_high_low/HighLowGame.java
+++ src/_01_else_if/_3_high_low/HighLowGame.java
@@ -1,46 +1,148 @@
-//(I can't save)
+//(I can't save)i already finished but my work didn't save
+// i saved it for you.  i'll put back the code you did...
+// but i don't think it's finished.  hold on a moment while i paste
+// in your code....
+// ok look over what you did...
+// alright - ready to work on this some more? you there?yes
+// ok, first, you generated a number in whwat range?  see
+// line 21 99+1=100 or (100)=1 =100
+// hi i'm back - I was helping Jay 
+// the range is 1 to 100.  it could help to tell the user
+// this ... i'll show you
+//(STOPPED HERE)
+// Generated random number between 1..100.  Continue working on program.
 package _01_else_if._3_high_low;
 
 import java.util.Random;
+
+import javax.swing.JOptionPane;
 
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		
-		int random = new Random().nextInt(5);//What is .nextInt(5)
-	
-		Random m = new Random();
-		// 2. Print out the random variable above
-		System.out.println(random);
-		
-		// 11. Repeat steps 1 to 10 ten times?
-		for(int n = 1; n <= 10; n++) {
 
-		}
-			// 1. Ask the user for a guessing a pop-up window, and save their response 
-			random = 90;
-			String example = "User's answer";
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			int n = Integer.parseInt(example);
+		int random = new Random().nextInt(100)+1;// YEAH!!
+		// can we review this?yes.  so nextInt(5) will generate one of these 5 numbers: 0 or 1 or 2 or 3 or 4.
+		// see how starting to count from 0 means that it won't generate the number 5?  yes
+		// ok, so if I change it to nextInt(3), what possible numbers will be generated?: 0, 1, 2.  good.
+		// now let's say I want to generate numbers in a range starting from 1, NOT 0.  I will add 1 like this:
+		// nextInt(3) + 1  ... what possible numbers will be generated in this case?: 0,1,2,3. good try but no.
+		// first, nextInt(3) will generate a number and then 1 is added to it... so what possible numbers will be generated 
+		// not sure?1,2,3  yes!!  so now, the program wants you to generate a random number between 1 - 100.
+		// any guess as to how to write the code? ...  in other words, we need to modify line 13: 
+
+		// you can run the program and see below what number is generated.  i'm going to check in with zach, then come
+		// back to help you save your code
+		System.out.println(random);
+		// 2. Print out the random variable above  
+
+		// 11. Repeat steps 1 to 10 ten times?
+		
+		// what i did was put all the code below inside
+		// this for loop.  first it asks the user
+		// for their guess and then it decides whether
+		// the guess is coreect or too high or too low.
+		// it needs a little fixing but let's see whawt it
+		// does so far.  try it!
+		
+		// ok.  so it shouldn't say you lose until when?
+		//3 times have passed and were wrong.  correct.
+		// so lets fix 2 things.  first, make the loop run
+		// 3 times. know how to fix that?  how many times
+		// does the loop run currently? no because i lets
+		//you go more than 3 times and said you losewin or low
+		//im so sorry ill be back ok im back sorry no prob
+		// so look at line 57 - how many times does the loop
+		// repeat?10   roioogoht.  but we said we'll give
+		// the user 3 chances, not 10
+		
+		// good.  now one more fix.  show we say 'you lose'
+		// after *each* guess or only after 3 wrong guesses?
+		//after 3 wrong guesses.  right. so that code
+		// should be *after* the for loop is done.  make
+		// sense?
+		
+		// 
+		for(int m = 1; m <= 3; m++) {
+	// line 69 doesn't make sense, it's saying if the
+			// user gueesses the number 3...not if the
+			// user made 3 wrong guesses.ok 
+			// 1. Ask the user for a guess using a pop-up window, and save their response 
+			String user = JOptionPane.showInputDialog("Guess a number between 1 and 100");	// make sense?
+
+			// 4. Convert the users\u2019 answer to an int (Integer.parseInt(string))...what did you name the string?
+			// on line 21 user.  good.  now i'd give the int a more descriptive name.  it's their guess.?
+			// do you understand the HighLowGame that you are working on?no  guess a secret number between 1 and 100,etc
+			//50  ok lets try playing the game, you and me, without the program.  i'm thinking of a secret number
+			// between 1 and 10.  take a guess .. 2.  too low.  try again.  7.  yeah you got it!
+			// do you understand the game now?  you keep guessing until you get the right number.  if it's not
+			// the right number you give a hint - too hi or too low.  at the top of the code, you'll generate
+			// the secret random number.  then your code will let the user guess the number.  make sense?yes
+			int guess = Integer.parseInt(user);
+
+			// ok here is the user's guess.  can we
+			// change the name of the variable from 'string'
+			// to 'guess'?  it makes more sense to me
+			// can I do that?change the name yes mo bc its calling 
+			//  that means I'm asking you if i can
+			// change the name in all those places....
+			//yes
+
+			// ok next.  we want to compare 'guess' to
+			// the randomly generated number, NOT to 50
+			// do you understand why? no
+			// right now the code compares the user's
+			// guess to 50, meaning that the "correct answer"
+			// is always 50 ... that's a boring game!!
+			// we want the correct answer to be a random number
+			// between 1 and 100.  make sense?yes.  ok
+			// the random number is stored ina variable called 'random'
+			// how can we use that variable in the code below?
+			//
+			// good!  now we'd like to change the code
+			// so the 
+			// user can try guessing a few times, maybe loop3 times?
+			// so we have to put some of this code in a loop
+
+
+			// can i help you a bit?  ok?
+			//yes..
 			// 5. if the guess is correct
-		if(random == 90)
+			if(guess == random) {///FIX THIS
 				// 6. Win
-		System.out.println("Win");
-				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
+				System.out.println("Win");
+			// great!  almost done.
+			// if the user guess the correct answer,
+			// should they be allowed to keep guessing
+			// or should the game end?end
+				System.exit(0);  // ok lets try the game
+				
+				// do you think it works ok?yes
+				// good. i want to help you save work.
+				// first i'm going to add back your
+				// crazy cat lady code.  then hopefully
+				// we can make a temp token in github
+				
+				// can you log into your github account?
+			}
+			// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
 			// 7. if the guess is high
-		//else if (90)
+			else if (guess >random)
 				// 8. Tell them it's too high
-		System.out.println("It's too high");
+				System.out.println("It's too high");
 			// 9. if the guess is low
-		//else if
+			else if (guess <random)
 				// 10. Tell them it's too low
-		System.out.println("It's too low");
-		// 13. Tell them they lose
+				System.out.println("It's too low");
+	// this line below should be moved to *after* the loop
+			// 13. Tell them they lose
+			// should this stay here or go away?:
+			//System.out.print("You lose");
+		} // here is the end of the for loop
+		
+		// put 'you lose' here:
 		System.out.print("You lose");
 	}
 
 }
-
-
-
