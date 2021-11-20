7d44bbc63b72c08150cf90855542050ff87f3c0b
student <student@ip-172-31-61-254.us-west-2.compute.internal>
2021-11-20 11:25:52 -0800

    started looking at YearsAlive

---
+++ src/YearsAlive.java
@@ -0,0 +1,23 @@
+
+public class YearsAlive {
+	// you need a main()
+	// and inside main, a loop that prints out all your years alive
+	
+	// questions?
+	
+	Joption.showmessagedialog (How many years do you have on earth.")
+}
+/*
+yes and the internet is slow making my screan glitch im trying so sorry
+ok sorry to hear.  for my info, is the computer you are using today one that has a touch screeen?
+		yes i could respond bc 
+		on the zoom chat it said u r not in
+		i'm here and i'm in zoom chat!  yes but still had it as if u were in the main room
+		weird!  well, we are able to chat here.
+		
+		what year were you born?  i was born in 1960.  so if I was making this YearsAlive
+				code, it would print all the years from 1960 to 2021.
+				can you try writing the code?2006_---------------------------------
+/
+// you don't have to ask the user anything ... you can just print the years you personally have 
+// have been alive.  questions?
