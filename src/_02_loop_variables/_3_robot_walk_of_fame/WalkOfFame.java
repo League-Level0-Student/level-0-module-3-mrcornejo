dd087d3b983e408f38936f2a1f5f6c0d92b53a39
root <root@67564a35c854>
2021-12-11 19:32:36 +0000

    continuing walk of fame

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -1,7 +1,5 @@
 // CONTINUE NEXT CLASS
 
-// wnat to look at previous robot code to refresh your memory?
-// robot_color_chooser
 package _02_loop_variables._3_robot_walk_of_fame;
 
 import org.jointheleague.graphical.robot.Robot;
@@ -11,86 +9,113 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
-		// You also need to show the robot to see the result of this line.
-		// let's move speed control up here to go a bit faster..
+		// how's it going?  you there?
+		// btw "pseudocode" is not real code
+		// for example, when i say 'for (10 times)
+		// real code might look like this: for(int i = 1; i <=1-; i++)
+		
+		// so what concepts are missing? idk moving places bc
+		//it makes a star in the same place
+		// yes.  and when it moves to a new place, 
+		// should it draw that move?no so move, then pen down.
+		//once done . wyietsh star pen up, move, so on....
+		// yes, so since class is almost over, lets just
+		// put a comment in the code for this and save work...
+		// where does the comment go "move to next place"
+		// not sure? i'll show you.  ok? 
+		// can i help you save your work?ye
+		
 		rob.setSpeed(40);
-		// before line 17, turn how much.. .i think it messed it up
-		// no.  this worked:
+		for (int e=1; e<=10; e++) {
+			for (int p=1; p<=5; p++) { 
+				rob.penDown();
+				rob.move(100);	// my pseudocode
+				// said move forward a line length 100
+				// good ... somewhere you *should* put
+				// the pen down so it draws
+				rob.turn(144);
+			}
+			// pick pen up here and move to next place
+		}
+		/*
+			
 		rob.turn(270);
 		rob.move(230);
-		rob.turn(90);  // not here
-		//here  .. did the star get drawn already by here?
-		//no
-		// when this is done it would be helpful to turn the robot to face up instead of left
-		// can you add a turn here  good! look ok to you so far?yes
-		// ok. now after each star is drawn, we want to move the robot to a spot where
-		// it will eventually draw the next star. that nneds to happen where in the code?
+		rob.turn(90);  
 		
-		// can you identify the code that actually draws the star?
 		
-		// then this messed it up:
-//		rob.turn(270);
-//		rob.move(300);
-// can i talk?
-		// the robot starts off facing up the page so when you are doing line 15,
-		// it is moving *up*.  
 		
-		// you need it to face *left* before it starts moving.  make sense?
-		//but you need it to move *left*.  what can you do to
-		// fix that?
 		
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-		// I think it's a 5-point star...but how am i suppost to do a cone like figure
+		
 
 		rob.setSpeed(40);
 			rob.penDown();
-			
-			// just to be clear, can you identify a line number in the code where
-			// the star is completely finished?
-			for(int i=1; i<=5; i++) {
-				// no semicolon at end of 'For' line 
-			// here i think you should have a loop that repeats
-			// 5 times.  inside the loop it moves forward 30,
-				//what is the 30 for? the length of each line
-			// then turns 144
-				// try this and see what it looks like...
+// you created the name 'i' twice			
+			for(int m=1; m<=5; m++) {
 				
-					// this is inside the loop that goes 5 times.... do you want that?
 				rob.penDown();
 				rob.move(100);
-	// i think the pic is hiding under the robot!!...can i make 30 bigger to solve that?
-				// good idea try it
-						rob.turn(144);	// getting closer!idk
+	
+						rob.turn(144);	
 		// 3. Set the length of each line in the star to 30.
 			}
-			// it's done HERE ... after the for loop has completed
-			// do you understand why? because its closed by }
-			// yes.  but also because of what the for loop does here:
-			// for 5 times it: draws a line and turns 144 degrees
-			// only after this for loop completes is one full star done.  make sense?yes
-			
-			// ok.  so down here, let's move the robot to the right a bit to the next
-			// location to draw a star:
-			// it drew a line while moving.  if you want to get rid of it, do penUp() 
-			//yea but were would it go
+		
 				rob.penUp();
 				rob.turn(90);
-				rob.move(120);	// maybe move a bit further aw
-				// let me add code to make the bot face up again
+				rob.move(120);	
 				
-				// better!  now how can we separate the stars a bit more
-				
-				// yay!!  so class is almost over.  maybe next time we work on having
-				// 10 stars on the walk of fame, ok?ok
-				
+/*
+		// so apparently this section moves the robot
+		// to a good spot and draws one star.
+		// let's now cooment out this section and
+		// uncooment the section above
+		
+		/*
 				rob.turn(-90);
 				rob.penDown();
-				for(int i=1; i<=5; i++) {	// you need to add a } i
+				for(int r=1; r<=5; r++) {	
 				rob.move(100);
 				rob.turn(144);
-				
+				//it didn't work it made a line and for some
+				//it went over another star
+				// ok lets try to identify the original code that worked
+				// and see what we can do with it
+				// try commenting out the new code you
+				// just added.  
+				 * so that can be turn into a loop right
+				 * because when the robot stops it's a starting
+				 * point for another to begin.... am i wrong?
+				 * 
+				 * yeah, that's the gist.  now if i were
+				 * doing this program i would start with
+				 * pseudocode which is like an outline in
+				 * programming.  my pseudocode would look like
+				 this:
+		move to left side of screen
+		for (10 times) {//this is like the loop right
+			// this for loop below creates ONE star
+			 * ONE star has 5 lines in it oooo ok i g
+			 * et it
+			 * i want to work with another student for
+			 * a bit.  try working on the code yourself ok
+			for (5 times) { //? but what is this
+				draw a line 100 long
+				turn 144 degrees//this is to creat the star
+				YES
+			}
+			move a bit to the right to get to next star
+		}
+		
+		does this pseudocode make sense?im a bit confused
+		ok about what?
+		
+		
+		
+		//to comment
+				// out a section.  i'll show you an example
 				}
-				
+			*/	
 
 		/** THE CHALLENGE: **/
 		/*
